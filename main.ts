e6d008c82f82d608bd0e36502ac79a2e0d6c052d
chranychaul <130452965+chranychaul@users.noreply.github.com>
2023-04-17 09:43:57 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -52,8 +52,8 @@ input.onButtonPressed(Button.B, function () {
             `)
     }
 })
-basic.forever(function () {
+for (let index = 0; index < 3; index++) {
     basic.showIcon(IconNames.Heart)
     basic.pause(100)
     basic.showIcon(IconNames.SmallHeart)
-})
+}
