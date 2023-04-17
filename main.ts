0489f23c3fb2acfee2d93f5aff404fccc1450d22
chranychaul <130452965+chranychaul@users.noreply.github.com>
2023-04-17 09:47:54 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -25,6 +25,33 @@ input.onButtonPressed(Button.A, function () {
             `)
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # # . . .
+            # # # # #
+            # . # # #
+            . . # . #
+            . . # . #
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # # . .
+            # # # # #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            # # . # #
+            . # . # .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+    }
+})
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 4; index++) {
         basic.showLeds(`
@@ -52,8 +79,3 @@ input.onButtonPressed(Button.B, function () {
             `)
     }
 })
-for (let index = 0; index < 3; index++) {
-    basic.showIcon(IconNames.Heart)
-    basic.pause(100)
-    basic.showIcon(IconNames.SmallHeart)
-}
