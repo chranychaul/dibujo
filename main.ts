3d26be7db3d70b5af0ef18eac8c612245c81ade3
chranychaul <130452965+chranychaul@users.noreply.github.com>
2023-04-17 09:41:34 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,59 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # # . . .
+            . # . . .
+            . # . . #
+            . # # # .
+            . # . # .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            . . . . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            # . . . .
+            # # . . .
+            . # # # .
+            . . . # #
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # . . #
+            . # # # .
+            . # . # .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            # # . # .
+            . # # # #
+            . # # # #
+            . # . . #
+            `)
+    }
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+})
